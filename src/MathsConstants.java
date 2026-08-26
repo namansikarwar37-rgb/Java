55843426b7a7a0b5969808a96bebbb0d517f4cd0
Naman Sikarwar <namansikarwar37@gmail.com>
2026-08-26 10:40:42 +0530

    new

---
+++ src/MathsConstants.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class MathsConstants {
+    public static void main(String[] args) {
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2, 3); // pow= power (1st number to the power second number)
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.99);
+
+
+
+
+        System.out.println(result);
+    }
+}
